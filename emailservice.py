6515ae0fce7e6739daa6eedfb85b80f5861ab3e1
roman <romanze1706@gmail.com>
2023-05-09 20:20:52 +0300

    Добавлен почтовый сервис

---
+++ emailservice.py
@@ -0,0 +1,14 @@
+import smtplib
+from email.message import EmailMessage
+
+
+def send_email(text):
+    msg = EmailMessage()
+    msg.set_content(text)
+    msg['Subject'] = 'MinimalRecords'
+    msg['From'] = 'botsuperlager@gmail.com'
+    msg['To'] = 'romanze1706@gmail.com'
+    smtp_obj = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    smtp_obj.login('botsuperlager@gmail.com', 'zcvdytjovtdjzuxk')
+    smtp_obj.send_message(msg)
+    smtp_obj.quit()
---
+++ service.py
@@ -0,0 +1,18 @@
+class Service:
+    chat_id = None
+    full_name = None
+    phone_number = None
+    email = None
+    description = None
+
+    def __init__(self, chat_id=None, full_name=None, phone_number=None, email=None, description=None):
+        self.chat_id = chat_id
+        self.full_name = full_name
+        self.phone_number = phone_number
+        self.email = email
+        self.description = description
+
+    def get_info(self):
+        s = "Заявка на услугу:\n Чат id " + str(self.chat_id) + '\n' + "ФИО\n" + self.full_name +\
+            "\nНомер телефона \n" + self.phone_number + '\n' + "email\n" + self.email + "\nОписание услуги\n" + self.description
+        return s
